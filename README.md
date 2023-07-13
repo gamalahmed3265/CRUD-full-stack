858beb1ceef463ffffb79fc5da538d293d2e32c9
Gamal Ahmed <gamalahmed3265@gmail.com>
2023-07-13 14:34:02 +0300

    add post functions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CRUD-full-stack
+
+
+#### if there auth problem:
+```SQL
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql.native.password BY 'root123';
+```
--- server/index.js
+++ server/index.js
@@ -6,33 +6,53 @@ const app = express()
 
 
 //middlewares
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
-  });
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Credentials", true);
+//   next();
+// });
 
 
 app.get("/", (req, res) => {
-    console.log("hello server");
-    res.json("hello server");
+  console.log("hello server");
+  res.json("hello server");
 });
 db.connect((err) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-  
-    console.log('Connected to MySQL!');
-  });
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('Connected to MySQL!');
+});
 app.get("/books", (req, res) => {
-    const quary = "SELECT * FROM books";
-    db.query(quary, (err, data) => {
-        if (err) return res.json(err.message)
-        return res.json(data);
-    })
+  const quary = "SELECT * FROM books";
+  db.query(quary, (err, data) => {
+    if (err) return res.json(err.message)
+    return res.json(data);
+  })
 })
 
+app.post("/books", (req, res) => {
+  const quary = "INSERT INTO books (`title`,`desc`,`cover`) VALUES (?) ";
+  // console.log(req.body);
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.cover
+  ];
+  db.query(quary, [values], (err, data) => {
+    if (err) {
+      return res.json(err)
+    }
+    return res.json(data)
+  })
+
+  // res.json("y");
+})
+
+
 app.listen(8800, () => {
-    console.log("server is runing");
+  console.log("server is runing");
 })
 

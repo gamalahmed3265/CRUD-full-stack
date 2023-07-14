3f8ec5176257165aa61271dd5c2aa22654f939af
Gamal Ahmed <gamalahmed3265@gmail.com>
2023-07-14 09:24:35 +0300

    add functions add book , update

--- site/src/App.js
+++ site/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Books from './pages/Books';
+import Update from './pages/Update';
+import Add from './pages/Add';
+
 function App() {
   return (
     <div className="App">
-      Hello
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books/>}/>
+          <Route path="/update" element={<Update/>}/>
+          <Route path="/add" element={<Add/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- site/src/pages/Add.jsx
+++ site/src/pages/Add.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Add = () => {
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    cover: "",
+    price: ""
+  });
+
+  const handleChanage = (e) => {
+    setBook(prev=>({...prev,[e.target.name]:e.target.value}));
+  }
+  const navigate=useNavigate();
+
+  const handleClick=async (e)=>{
+    e.preventDefault();
+    try{
+      console.log(book);
+      await axios.post("http://localhost:8800/books",book);
+      navigate("/");
+    }catch(err){
+      console.log(err);
+    }
+  }
   return (
-    <div>Add</div>
+    <div className='form'>
+      <h3>add Book</h3>
+      <input type="text" name="title" onChange={handleChanage} placeholder='title' />
+      <input type="text" name="desc" onChange={handleChanage} placeholder='desc' />
+      <input type="text" name="price" onChange={handleChanage} placeholder='price' />
+      <input type="text" name="cover" onChange={handleChanage} placeholder='cover' />
+
+      <button onClick={handleClick}>Add</button>
+    </div>
   )
 }
 
